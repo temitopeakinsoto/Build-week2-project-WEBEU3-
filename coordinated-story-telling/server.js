4637c07df811debe21b528b83ea74ce345eb4085
Adetunji Awobajo <surfsamson@gmail.com>
2019-09-28 06:36:10 -0700

    feat: Create a dummy api server
    In this commit, i created a dummy Api/server for getting dummy stories. The stories gotten
    from this api is then used to create a list of stories from users.
    
    This api endpoint is temporary and should be replaced with the endpoint provided by
    the backend devs of the coordinated-story-telling team

---
+++ coordinated-story-telling/server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const CORS = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(CORS()); 
+
+const stories = [
+	{
+		id: 0,
+		title: 'My Lambda Experience',
+		author: 'Temitope Akinsoto',
+		myStory: "The client agrees that the operation of the account is subject to compliance by Lead Asset Management with all laws, regulations, administrative rules and orders which may from time to time be   authorized by the Federal Government of Nigeria and/or any other regulatory authorities in Nigeria.  b. 2 In consideration of Lead Asset Management allowing the client to operate the account from time to time, the client hereby undertakes to hold Lead Asset Management harmless and keep Lead Asset Management indemnified from all losses, costs, or damages Lead Asset Management may sustain or be put to. ",
+	},
+	{
+		id: 1,
+		title: 'How I learnt how to Invest',
+		author: 'Femi Anjorin',
+		myStory: "The client agrees that the operation of the account is subject to compliance by Lead Asset Management with all laws, regulations, administrative rules and orders which may from time to time be   authorized by the Federal Government of Nigeria and/or any other regulatory authorities in Nigeria.  b. 2 In consideration of Lead Asset Management allowing the client to operate the account from time to time, the client hereby undertakes to hold Lead Asset Management harmless and keep Lead Asset Management indemnified from all losses, costs, or damages Lead Asset Management may sustain or be put to. ",
+	},
+	{
+		id: 2,
+		title: 'How I learnt how to code',
+		author: 'David Kuseh',
+		metascore: "The client agrees that the operation of the account is subject to compliance by Lead Asset Management with all laws, regulations, administrative rules and orders which may from time to time be   authorized by the Federal Government of Nigeria and/or any other regulatory authorities in Nigeria.  b. 2 In consideration of Lead Asset Management allowing the client to operate the account from time to time, the client hereby undertakes to hold Lead Asset Management harmless and keep Lead Asset Management indemnified from all losses, costs, or damages Lead Asset Management may sustain or be put to. ",
+	},
+	{
+		id: 3,
+		title: 'Write a blog post today',
+		author: 'Alison Ludick',
+		myStory: "The client agrees that the operation of the account is subject to compliance by Lead Asset Management with all laws, regulations, administrative rules and orders which may from time to time be   authorized by the Federal Government of Nigeria and/or any other regulatory authorities in Nigeria.  b. 2 In consideration of Lead Asset Management allowing the client to operate the account from time to time, the client hereby undertakes to hold Lead Asset Management harmless and keep Lead Asset Management indemnified from all losses, costs, or damages Lead Asset Management may sustain or be put to. ",
+	},
+	{
+		id: 4,
+		title: 'How I made money trading online',
+		author: 'The Farely Brothers',
+		myStory: "The client agrees that the operation of the account is subject to compliance by Lead Asset Management with all laws, regulations, administrative rules and orders which may from time to time be   authorized by the Federal Government of Nigeria and/or any other regulatory authorities in Nigeria.  b. 2 In consideration of Lead Asset Management allowing the client to operate the account from time to time, the client hereby undertakes to hold Lead Asset Management harmless and keep Lead Asset Management indemnified from all losses, costs, or damages Lead Asset Management may sustain or be put to. ",
+	},
+	{
+		id: 5,
+		title: 'Learn React in 20 days',
+		author: 'Justin Irabor',
+		myStory: "The client agrees that the operation of the account is subject to compliance by Lead Asset Management with all laws, regulations, administrative rules and orders which may from time to time be   authorized by the Federal Government of Nigeria and/or any other regulatory authorities in Nigeria.  b. 2 In consideration of Lead Asset Management allowing the client to operate the account from time to time, the client hereby undertakes to hold Lead Asset Management harmless and keep Lead Asset Management indemnified from all losses, costs, or damages Lead Asset Management may sustain or be put to. ",
+	},
+];
+
+app.get('/api/stories', (req, res) => {
+	res.send(stories);
+});
+
+app.get('/api/stories/:id', (req, res) => {
+	const story = stories.filter(story => story.id.toString() === req.params.id)[0];
+	res.status(200).json(story);
+});
+
+app.listen(5000, () => {
+	console.log('Server listening on port 5000');
+});

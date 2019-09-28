ec314bed710f9c2bd3dbe9e7b3653d13d3ea4421
Adetunji Awobajo <surfsamson@gmail.com>
2019-09-28 15:51:18 -0700

    REFACTOR: Modify <Story /> component
    In this commit, the story component is modified such that it makes an ajax call to get details about each story.
    By using the props.match.params.id of each individual story being linked from the <Stories /> component, it makes
    a new axios call to the server for the story with the corresponding id and renders the information via the
    <Storiesdetails/> component

--- coordinated-story-telling/src/components/Story.js
+++ coordinated-story-telling/src/components/Story.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {Link} from "react-router-dom";
+import Storiesdetails from './Storiesdetails';
+import styled  from 'styled-components';
+import axios from "axios";
 
+const storyCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding-left: 20px;
+
+`;
 function Story(props) {
-    console.log('Story id is', props.match.params.id);
-    return(
-        <div>
-            <h2>This the individual story page</h2>
-        </div>
+    const id = props.match.params.id;
+    const [story, setStory] = useState([]);
+    useEffect(() => {
+        axios
+        .get(`http://localhost:5000/api/stories/${id}`)
+        .then(response => {
+            setStory(response.data); 
+        }) 
+        .catch(error => {
+            console.log("There was an error with your request", error)
+        });
+    }, []);
+
+    return ( 
+        <div> 
+            <Link to="/stories">back</Link>           
+            <Storiesdetails key={story.id} title={story.title} author={story.author} myStory={story.myStory}/>             
+        </div>            
     )
 }
 
